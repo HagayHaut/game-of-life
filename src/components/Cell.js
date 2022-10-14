84e8fac20adb4881a94420129bfdcccbd55ec249
Hagay Haut <hagayhaut@gmail.com>
2022-10-14 12:36:32 -0400

    update cell colors to black and yellow

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -3,8 +3,8 @@ import React from "react";
 function Cell({ alive, coors, onCellClick }) {
     return (
         <div
-            className={`w-5 h-5 border border-black ${
-                alive ? "bg-cyan-300" : null
+            className={`w-5 h-5 border border-slate-900 ${
+                alive ? "bg-yellow-300" : "bg-black"
             }`}
             onClick={() => onCellClick(...coors)}
         />
